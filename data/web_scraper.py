04d3d29fff559d25694a7a8b413a431e8e8b7b90
Allison Tao <allisontao24@gmail.com>
2023-07-20 23:49:57 -0400

    update raw data csv

--- data/web_scraper.py
+++ data/web_scraper.py
@@ -91,7 +91,7 @@ def main():
     
     keys = res[0].keys()
 
-    with open('raw_data.csv', 'w', newline='') as output_file:
+    with open('data/raw_data.csv', 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(res)
