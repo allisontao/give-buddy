e23f5cb81af8af2b4c816518cfee7d47e982c8a6
kerry918 <kerryliukerui@gmail.com>
2024-01-02 14:05:36 -0500

    add basic auth function

---
+++ frontend/src/page/Signup.jsx
@@ -0,0 +1,118 @@
+import React, {useState} from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { auth } from '../firebase';
+ 
+const Signup = () => {
+    const navigate = useNavigate();
+ 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+ 
+    const onSubmit = async (e) => {
+      e.preventDefault()
+     
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+            navigate("/login")
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+        });
+ 
+   
+    }
+ 
+  return (
+    <main >        
+        <section>
+            <div>
+                <div>                  
+                    <h1> Give Buddy </h1>                                                                            
+                    <form>   
+                        <div>
+                            <label htmlFor="first-name">
+                                First Name
+                            </label>
+                            <input
+                                id="first-name"
+                                name="first-name"
+                                type="text"                                    
+                                required                                                                                
+                                placeholder="FirstName"
+                                onChange={(e)=>setFirstName(e.target.value)}
+                            />
+                        </div>  
+                        <div>
+                            <label htmlFor="last-name">
+                                Last Name
+                            </label>
+                            <input
+                                id="last-name"
+                                name="last-name"
+                                type="text"                                    
+                                required                                                                                
+                                placeholder="LastName"
+                                onChange={(e)=>setLastName(e.target.value)}
+                            />
+                        </div>                                                                                          
+                        <div>
+                            <label htmlFor="email-address">
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                label="Email address"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}  
+                                required                                    
+                                placeholder="Email address"                                
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                label="Create password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required                                 
+                                placeholder="Password"              
+                            />
+                        </div>                                             
+                        
+                        <button
+                            type="submit" 
+                            onClick={onSubmit}                        
+                        >  
+                            Sign up                                
+                        </button>
+                                                                     
+                    </form>
+                   
+                    <p>
+                        Already have an account?{' '}
+                        <NavLink to="/login" >
+                            Sign in
+                        </NavLink>
+                    </p>                   
+                </div>
+            </div>
+        </section>
+    </main>
+  )
+}
+ 
+export default Signup
