271cecacbab3a6be87bf857cca8d18059a7de8d4
kerry918 <kerryliukerui@gmail.com>
2024-01-05 16:00:30 -0500

    step four polish

--- frontend/src/page/Onboarding/StepFour.tsx
+++ frontend/src/page/Onboarding/StepFour.tsx
@@ -4,11 +4,21 @@ import "./StepFour.css"
 const StepFour = () => {
   const choices = ["Within Canada", "Within my province", "Within my city", "Doesn’t matter to me"]
   const [showProvince, setShowProvince] = React.useState(false)
+  const [showCity, setShowCity] = React.useState(false)
   const [location, setLocation] = React.useState("")
 
   const handleClick = (choice: string) => {
     if (choice === "Within my province") {
+      setShowCity(false)
       setShowProvince(true)
+    } 
+    else if (choice === "Within my city") {
+      setShowProvince(false)
+      setShowCity(true)
+    }
+    else {
+      setShowCity(false)
+      setShowProvince(false)
     }
     setLocation(choice)
   }
@@ -18,12 +28,22 @@ const StepFour = () => {
       {choices.map((choice) => {
         return (
           <>
-            <div id="onboarding-step-four-button" onClick={() => handleClick(choice)}>
+            <div 
+              id="onboarding-step-four-button" 
+              onClick={() => handleClick(choice)}
+              style={{backgroundColor: location === choice ? "#F2F2F2": "white", border: location === choice ? "3px solid #BEBEBE" : "1px solid #BEBEBE"}}
+            >
               <p id="onboarding-step-four-button-text">{choice}</p>
             </div>
             {(choice === "Within my province" && showProvince === true) && (
               <>
                 <p id="onboarding-step-four-subtext">Enter Province</p>
+                <input id="onboarding-step-four-input"/>
+              </>
+            )}
+            {(choice === "Within my city" && showCity === true) && (
+              <>
+                <p id="onboarding-step-four-subtext">Enter City</p>
                 <input id="onboarding-step-four-input"/>
               </>
             )}
