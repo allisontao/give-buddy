6cd10425aef26584460d4ddfee32950ba49622cb
kerry918 <kerryliukerui@gmail.com>
2024-01-14 21:23:55 -0500

    fixes

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -3,13 +3,17 @@ import { createWithEqualityFn } from 'zustand/traditional'
 
 interface GiveBuddyState {
   category: string[]
+  subcategory_list: string[]
 }
 
 type GiveBuddyAction = {
   updateCategory: (nav: GiveBuddyState['category']) => void
+  updateSubcategory: (category: GiveBuddyState['subcategory_list']) => void
 }
 
 export const useGiveBuddyStore = createWithEqualityFn<GiveBuddyState & GiveBuddyAction>()((set) => ({
   category: [],
+  subcategory_list: [],
   updateCategory: (newCategory) => set(() => ({ category: newCategory })),
+  updateSubcategory: (newSubcategory) => set(() => ({ subcategory_list: newSubcategory}))
 }), shallow)
