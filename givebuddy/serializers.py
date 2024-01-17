aba9764444027e98d803c0ab0844fd8c52a054be
nadine.lin <nadine0119lin@gmail.com>
2024-01-17 01:27:27 -0500

    modify endpoint for matching algo

--- givebuddy/serializers.py
+++ givebuddy/serializers.py
@@ -4,6 +4,8 @@ from .models import User
 class Onboarding_serializer(serializers.Serializer):
     categories = serializers.ListField(max_length=255, required=False)
     subcategories = serializers.ListField(max_length=255, required=False)
+    province = serializers.CharField(required=False, max_length=100, allow_blank=True)
+    city = serializers.CharField(required=False, max_length=100, allow_blank=True)
     ft_ranking = serializers.IntegerField(min_value=1)
     rr_ranking = serializers.IntegerField(min_value=1)
     ctc_ranking = serializers.IntegerField(min_value=1)
