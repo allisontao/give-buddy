3fb3c35bfb34dab81bb4fcfe4a1b10069628ca19
nadine.lin <nadine0119lin@gmail.com>
2024-01-21 15:09:45 -0500

    user endpoints

--- backend/urls.py
+++ backend/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('my_donated_charities/<str:user_id>', views.my_donated_charities, name='my_donated_charities'),
     path('update_donated_charities/<str:user_id>', views.update_donated_charities, name='update_donated_charities'),
     path('matched_for_you/<str:user_id>/', views.matched_for_you, name='matched_for_you'),
-    path('registration', views.user_registration, name='user_registration')
+    path('registration', views.user_registration, name='user_registration'),
+    path('user_profile/<str:user_uid>', views.user_info, name='user_info')
 ]
